69f365e31dfa510f7e3b6f87e64856c74075c907
Agustin Lascialandare <lascialandare@gmail.com>
2015-08-13 19:41:09 -0300

    add minified version

--- jquery-smart-sidebar.js
+++ jquery-smart-sidebar.js
@@ -16,14 +16,15 @@
 
         var $elem = $(elem);
         $elem.data('SmartSidebar', true);
+       
 
         var offset = $elem.offset();
         var height = $elem.height();
 
         var $rail = $(
-            '<div class="' + (options.railClass || 'fixed-rail') + 
-            '" style="overflow:hidden;position:fixed;padding:1px;"><div class="top-offset" style="height:' +
-            offset.top + 'px;"></div></div>' );
+            '<div class="' + 
+            (options.railClass || 'fixed-rail') + 
+            '" style="overflow:hidden;position:fixed;padding:1px;"><div class="top-offset"></div></div>');
 
         $rail.css({left: offset.left+'px'});
         if (!isNaN(options.top)) {
@@ -38,6 +39,9 @@
 
         var $window = $(window);
         var lastScrollTop = $window.scrollTop();
+        var topOffsetHeight = Math.max(offset.top - $rail.offset().top - lastScrollTop, 0);
+        
+        $rail.find('.top-offset').css({ height: topOffsetHeight + 'px' });
         
         $window.scroll(function(){
             var scrollTop = $window.scrollTop();
@@ -45,17 +49,16 @@
             var railScrollTop = $rail.scrollTop();
             var scroll = railScrollTop + diff;
             lastScrollTop = scrollTop;
-            if (scroll < offset.top) {
-                if (scrollTop < offset.top) {
+            if (scroll < topOffsetHeight) {
+                if (scrollTop < topOffsetHeight) {
                     scroll = scrollTop
                 } else {
-                    scroll = offset.top;
+                    scroll = topOffsetHeight;
                 }
             }
+           
             $rail.scrollTop(scroll);
         });
-
-        return $elem;
     };
 
     $.fn.smartSidebar = function(options) {
